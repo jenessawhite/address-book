8c0623e154ba3b22f1b7bf0a564951a50cead82f
Jenessa White <jenessaw31@gmail.com>
2021-04-28 10:42:38 -0500

    contacts migration

---
+++ db/migrate/20210428152955_create_contacts.rb
@@ -0,0 +1,13 @@
+class CreateContacts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :phone_number
+      t.string :email_address
+      t.date :birthday
+      t.string :company
+
+      t.timestamps
+    end
+  end
+end
