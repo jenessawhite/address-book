63a5f424dfa5d4592bd35c72f4b2a1755b222cf0
Jenessa White <jenessaw31@gmail.com>
2021-05-05 10:32:54 -0500

    add some contacts tests

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,8 +2,12 @@ require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get contacts_index_url
+    get contacts_url
     assert_response :success
   end
 
+  test "should return contacts" do
+    get contacts_url
+    assert_equal Contact.all, assigns(:contacts)
+  end
 end
