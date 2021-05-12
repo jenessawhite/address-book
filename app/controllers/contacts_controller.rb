7a5945e582ae0ac9446eff1b6b977f5ad5656aed
Jenessa White <jenessaw31@gmail.com>
2021-05-12 10:31:26 -0500

    add different views and routes

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,40 @@ class ContactsController < ApplicationController
   def index
     @contacts = Contact.all
   end
+
+  def show
+    find_contact
+    # @contact = Contact.find_by!(id: params[:id])
+  end
+
+  def edit
+    find_contact
+    # @contact = Contact.find_by!(id: params[:id])
+  end
+
+  def update
+    find_contact
+    # @contact = Contact.find_by!(id: params[:id])
+  end
+
+  def new
+    @contact = Contact.new(contact_params)
+    redirect_to contacts_path(@contact.id) if @contact.save
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(
+      :id,
+      :name,
+      :phone_number,
+      :email_address,
+      :birthday,
+      :company
+    )
+  end
+
+  def find_contact
+    @contact = Contact.find_by!(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "contacts#index"
   get "/contacts", to: "contacts#index"
+  get "/contacts/:id", to: "contacts#show"
+  get "/contacts/new", to: "contacts#new"
+  get "/contacts/:id/edit", to: "contacts#edit"
+  put "/contacts/:id/update", to: "contacts#update"
 end
